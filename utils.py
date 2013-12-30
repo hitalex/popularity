a3733644839ce6b970fdc3faac1672982256b524
Qingchao Kong <kqingchao@gmail.com>
2013-12-30 15:59:06 +0800

    add downsampling routine

--- utils.py
+++ utils.py
@@ -8,6 +8,41 @@ def seg_chinese(chinese_str):
     seg_list = jieba.cut(chinese_str)
     return " ".join(seg_list)
     
+def down_sampling_dataset(dataset, comment_count_dataset, Bao_dataset, category_count_list):
+    """ 下采样技术：对有大多数样本的类进行下采样，使得两类的样本数相差不多
+    Note: 这里需要保证所有的方法使用的数据集相同，包括baseline方法和当前提出的方法
+    """
+    from random import random
+    ratio = category_count_list[0] * 1.0 / category_count_list[1]
+    if ratio < 1:
+        majority_category = 1
+    elif ratio > 1:
+        ratio = 1 / ratio
+        majority_category = 0
+    else:
+        return dataset, comment_count_dataset, Bao_dataset
+    
+    selected_index = []
+    new_category_count_list = [0, 0]
+    print 'Downsampling category: ', majority_category
+    total = len(dataset)
+    for i in range(total):
+        # TODO: 需要说明的是类别标签信息必须在最后一个item上，否则会出错
+        cat = dataset[i][-1]
+        assert(cat == 0 or cat == 1)
+        if cat == majority_category: # 只对majortiy class进行处理
+            if random() > ratio: # 未被选中
+                continue
+                
+        selected_index.append(i)
+        new_category_count_list[cat] += 1
+    
+    dataset = [dataset[i] for i in selected_index]
+    comment_count_dataset = [comment_count_dataset[i] for i in selected_index]
+    Bao_dataset = [Bao_dataset[i] for i in selected_index]
+    
+    return dataset, comment_count_dataset, Bao_dataset, new_category_count_list
+    
 def ratio_accuracy_distribution_plot(y_true, y_pred, test_set, group_id, factor_name):
     """
     查看对于不同的dynamic factor，它们在不同的ratio上的预测结果
